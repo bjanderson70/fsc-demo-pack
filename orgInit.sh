316ac0702f08f0fa18bf4fdaf05432a3878a8b1c
Vchalem <Vchalem@gmail.com>
2019-03-05 14:38:43 -0500

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -4,7 +4,7 @@
 sfdx force:org:create -f config/demo-scratch-def.json -a FSCADK2 --setdefaultusername -d 15
 
 #pckg installs
-sfdx force:package:install --package 04t1E000000lU07 -w 20 
+sfdx force:package:install --package 04t1E000000lU0M -w 20 
 #fdx force:package:install --package 04t1E000001Iql5 -w 20
 
 
