031d03e6242b260b5858c33e3bdf23af9b5973c6
Vanessa Chalem <vchalem@salesforce.com>
2019-03-05 11:40:54 -0500

    changing orgInit.sh for Heroku deployer

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sfdx force:org:create -f config/demo-scratch-def.json -a FSCADK2 -v Your-Dev-Hub -d 15
+sfdx force:org:create -f config/demo-scratch-def.json -a FSCADK2 -v my-hub-org -d 15
 
