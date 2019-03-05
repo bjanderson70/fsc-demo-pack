b089bc60b9665347ff6a8a4f7b3186f98e6a2588
Vchalem <Vchalem@gmail.com>
2019-03-05 13:25:21 -0500

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #create scratch org
-sfdx force:org:create -f config/demo-scratch-def.json -a FSCADK2 --setdefaultusername -d 15
+sfdx force:org:create -f config/demo-scratch-def.json -a FSCADK2 -d 15
 
 #pckg installs
 sfdx force:package:install --package 04t1E000000lU07 -w 20 
