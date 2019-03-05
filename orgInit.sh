593bfaaabca0844b580f434b2748bf5f84e29051
Vchalem <Vchalem@gmail.com>
2019-03-05 12:00:17 -0500

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-sfdx force:org:delete 
-
-#sfdx force:org:create -f config/demo-scratch-def.json -a FSCADK2 -v my-hub-org -d 15
+sfdx force:org:create -f config/demo-scratch-def.json -a FSCADK2 -v my-hub-org -d 15
 
